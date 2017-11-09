268b8b3c80ba3b4db2c7a1796a50347ebd3155dd
letrend <simon.trendel@tum.de>
2017-11-09 13:51:16 +0100

    draft ootx decoder

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,49 @@ int main(int argc, char *argv[]) {
     RoboyPlexus roboyPlexus(h2p_lw_myo_addr, h2p_lw_i2c_addr, deviceIDs, h2p_lw_darkroom_addr, h2p_lw_adc_addr);
 
     while(ros::ok()){
-        ROS_INFO_THROTTLE(5, "i'm nervous");
+        uint32_t crc32 = IORD(h2p_lw_darkroom_addr, 32);
+        uint32_t fw_version = IORD(h2p_lw_darkroom_addr, 33);
+        uint32_t ID = IORD(h2p_lw_darkroom_addr, 34);
+        uint32_t phase0 = IORD(h2p_lw_darkroom_addr, 35);
+        uint32_t phase1 = IORD(h2p_lw_darkroom_addr, 36);
+        uint32_t tilt0 = IORD(h2p_lw_darkroom_addr, 37);
+        uint32_t tilt1 = IORD(h2p_lw_darkroom_addr, 38);
+        uint32_t unlock_count = IORD(h2p_lw_darkroom_addr, 39);
+        uint32_t hw_version = IORD(h2p_lw_darkroom_addr, 40);
+        uint32_t curve0 = IORD(h2p_lw_darkroom_addr, 41);
+        uint32_t curve1 = IORD(h2p_lw_darkroom_addr, 42);
+        uint32_t acc_x = IORD(h2p_lw_darkroom_addr, 43);
+        uint32_t acc_y = IORD(h2p_lw_darkroom_addr, 44);
+        uint32_t acc_z = IORD(h2p_lw_darkroom_addr, 45);
+        uint32_t gibphase0 = IORD(h2p_lw_darkroom_addr, 46);
+        uint32_t gibphase1 = IORD(h2p_lw_darkroom_addr, 47);
+        uint32_t gibmag0 = IORD(h2p_lw_darkroom_addr, 48);
+        uint32_t gibmag1 = IORD(h2p_lw_darkroom_addr, 49);
+        uint32_t mode = IORD(h2p_lw_darkroom_addr, 50);
+        uint32_t faults = IORD(h2p_lw_darkroom_addr, 51);
+        ROS_INFO_THROTTLE(1,
+            "crc32:       %d\n"
+            "fw_version:  %d\n"
+            "ID:          %d\n"
+            "phase0:      %d\n"
+            "phase1:      %d\n"
+            "tilt0:       %d\n"
+            "tilt1:       %d\n"
+            "unlock_count:%d\n"
+            "hw_version:  %d\n"
+            "curve0:      %d\n"
+            "curve1:      %d\n"
+            "acc_x:       %d\n"
+            "acc_y:       %d\n"
+            "acc_z:       %d\n"
+            "gibphase0:   %d\n"
+            "gibphase1:   %d\n"
+            "gibmag0:     %d\n"
+            "gibmag0:     %d\n"
+            "mode:        %d\n"
+            "faults:      %d\n", crc32, fw_version,ID,phase0,phase1,tilt0,tilt1,unlock_count,hw_version,
+                          curve0,curve1,acc_x,acc_y,acc_z, gibphase0, gibphase1, gibmag0, gibmag1, mode, faults
+        );
     }
 
     // clean up our memory mapping and exit
