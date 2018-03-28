90b12b213ab6024f9511c2d9437fc37f37e6f2b7
Alona Kharchenko <aliona.kharchenko@gmail.com>
2018-03-29 00:13:58 +0200

    actionlib

--- cmake/findros.cmake
+++ cmake/findros.cmake
@@ -9,7 +9,9 @@ set(ros_LIBRARIES
         ${ROS_ROOT_DIR}/lib/libroscpp.so
         ${ROS_ROOT_DIR}/lib/librostime.so
         ${ROS_ROOT_DIR}/lib/libroscpp_serialization.so
+        ${ROS_ROOT_DIR}/lib/libactionlib.so
         ${CMAKE_CURRENT_SOURCE_DIR}/arm/usr/lib/arm-linux-gnueabihf/libboost_system.so
+        ${CMAKE_CURRENT_SOURCE_DIR}/arm/usr/lib/arm-linux-gnueabihf/libboost_thread.so
         ${CMAKE_CURRENT_SOURCE_DIR}/arm/usr/lib/arm-linux-gnueabihf/liblog4cxx.so
         ${CMAKE_CURRENT_SOURCE_DIR}/arm/usr/lib/arm-linux-gnueabihf/libboost_regex.so
         ${CMAKE_CURRENT_SOURCE_DIR}/arm/usr/lib/arm-linux-gnueabihf/libapr-1.so
