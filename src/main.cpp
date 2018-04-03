dfd1087e83a76dd67da0a755158b91691efc23d6
missxa <aliona.kharchenko@gmail.com>
2018-04-03 21:43:49 +0200

    put back actions

--- src/main.cpp
+++ src/main.cpp
@@ -145,10 +145,8 @@ int main(int argc, char *argv[]) {
 //
     RoboyPlexus roboyPlexus(myoControl, h2p_lw_myo_addr, h2p_lw_i2c_addr, h2p_lw_darkroom_addr, h2p_lw_darkroom_ootx_addr, h2p_lw_adc_addr);
 //
-////    auto nh2 = ros::NodeHandlePtr(new ros::NodeHandle);
-////    FibonacciAction fibonacci("fibonacci");
-////    PerformMovementAction performMovementAction(myoControl,"movement_server");
-////    PerformMovementsAction performMovementsAction(myoControl,"movements_server");
+    PerformMovementAction performMovementAction(myoControl,"movement_server");
+    PerformMovementsAction performMovementsAction(myoControl,"movements_server");
 //
     uint8_t mask = 0x1;
     ros::Rate rate(30);
