3d6acbbd2618a3d5380871f21e0d54c08c66523d
letrend <simon.trendel@tum.de>
2018-07-23 13:59:08 +0200

    with helper script

---
+++ scripts/control_hand.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo setpoint $1
+rostopic pub /roboy/middleware/HandCommand roboy_communication_middleware/HandCommand "id: 0
+setPoint: [$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1,$1]  
+motorid: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]"
