5f4f6e43176354000819c06282fefa6f42b7b26c
letrend <simon.trendel@tum.de>
2018-11-07 10:13:07 +0100

    with tlv

--- include/roboy_plexus/tlv493d.hpp
+++ include/roboy_plexus/tlv493d.hpp
@@ -1,3 +1,37 @@
+/*
+    BSD 3-Clause License
+
+    Copyright (c) 2017, Roboy
+            All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+            modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+            IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+            FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+            DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+            CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    author: Simon Trendel ( st@gi.ai ), 2018
+    description: Class for configuring and reading 3d magnetic sensor TLV493d
+*/
 #pragma  once
 
 #include <stdint.h>
@@ -26,10 +60,9 @@ public:
     TLV493D(void *i2c_base, vector<uint8_t> &deviceAddress, vector<int> &devicePin);
     ~TLV493D();
     bool initTLV(uint8_t &deviceaddress, int devicepin);
-    float convertToMilliTesla(uint8_t data);
-    void readTLV_B_MSB(int deviceaddress, vector<uint8_t> &data);
+    float convertToMilliTesla(uint8_t MSB, uint8_t LSB) ;
     void readAllRegisters(int deviceaddress, vector<uint8_t> &reg, bool print=true);
-    void read(vector<float> &x, vector<float> &y, vector<float> &z);
+    bool read(float &fx, float &fy, float &fz);
 private:
     vector<uint8_t> deviceAddress;
     uint8_t gpioreg = 0;
