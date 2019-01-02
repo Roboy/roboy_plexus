d06c0c19caf696b11df352f39e9f70eee7441b0c
letrend <simon.trendel@tum.de>
2019-01-02 21:25:04 +0100

    with lighthouse tracking for msj_platform

--- src/msj_platform.cpp
+++ src/msj_platform.cpp
@@ -51,8 +51,8 @@ using namespace std;
 #define HW_REGS_SPAN ( 0x04000000 )
 #define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
 
-MSJPlatform::MSJPlatform(int32_t *msj_platform_base, int32_t *switch_base, vector<int32_t*> i2c_base):
-        msj_platform_base(msj_platform_base),switch_base(switch_base),i2c_base(i2c_base){
+MSJPlatform::MSJPlatform(int32_t *msj_platform_base, int32_t *switch_base, vector<int32_t*> i2c_base, int32_t*darkroom_base, int32_t*darkroom_ootx_base):
+        msj_platform_base(msj_platform_base),switch_base(switch_base),i2c_base(i2c_base),darkroom_base(darkroom_base),darkroom_ootx_base(darkroom_ootx_base){
     if (!ros::isInitialized()) {
         int argc = 0;
         char **argv = NULL;
@@ -98,6 +98,18 @@ MSJPlatform::MSJPlatform(int32_t *msj_platform_base, int32_t *switch_base, vecto
 
     magnetic_thread = boost::shared_ptr<std::thread>( new std::thread(&MSJPlatform::publishMagneticSensors, this));
     magnetic_thread->detach();
+
+    darkroom_pub = nh->advertise<roboy_middleware_msgs::DarkRoom>("/roboy/middleware/DarkRoom/sensors",
+                                                                  1);
+    darkroom_ootx_pub = nh->advertise<roboy_middleware_msgs::DarkRoomOOTX>(
+            "/roboy/middleware/DarkRoom/ootx", 1);
+    darkroom_status_pub = nh->advertise<roboy_middleware_msgs::DarkRoomStatus>(
+            "/roboy/middleware/DarkRoom/status", 1);
+    darkroom_thread = boost::shared_ptr<std::thread>(new std::thread(&MSJPlatform::darkRoomPublisher, this));
+    darkroom_thread->detach();
+    darkroom_ootx_thread = boost::shared_ptr<std::thread>(
+            new std::thread(&MSJPlatform::darkRoomOOTXPublisher, this));
+    darkroom_ootx_thread->detach();
 }
 
 void MSJPlatform::publishStatus(){
@@ -185,11 +197,177 @@ bool MSJPlatform::Zero(std_srvs::Empty::Request &req, std_srvs::Empty::Response 
     return true;
 }
 
+void MSJPlatform::darkRoomPublisher() {
+    ros::Rate rate(240);
+    high_resolution_clock::time_point t0 = high_resolution_clock::now();
+    while (ros::ok()) {
+        uint active_sensors = 0;
+        roboy_middleware_msgs::DarkRoom msg;
+        msg.object_id = ethaddr;
+        for (uint i = 0; i < NUM_SENSORS; i++) {
+            int32_t val = IORD(darkroom_base, i);
+            high_resolution_clock::time_point t1 = high_resolution_clock::now();
+            microseconds time_span = duration_cast<microseconds>(t1 - t0);
+            msg.timestamp.push_back(time_span.count());
+            msg.sensor_value.push_back(val);
+            if ((val >> 29) & 0x1)//valid
+                active_sensors++;
+        }
+        ROS_INFO_THROTTLE(5,"lighthouse sensors active %d/%d", active_sensors, NUM_SENSORS);
+        darkroom_pub.publish(msg);
+        rate.sleep();
+    }
+}
+
+void MSJPlatform::darkRoomOOTXPublisher() {
+    // ootx frames are 17bits preamble + 271 bits payload + 32 bits checksum = 320 bits.
+    // At 120Hz motor speed, this is ~2.6 seconds per frame. Lets set the update rate to 5 seconds then.
+    ros::Rate rate(1 / 5.0);
+    high_resolution_clock::time_point t0 = high_resolution_clock::now();
+    while (ros::ok()) {
+        bool successfully_decoded_ootx = true;
+        for (uint lighthouse = 0; lighthouse < 2; lighthouse++) {
+            // TODO: remove reverse and have the fpga convert it directly
+            ootx.frame.fw_version = (uint16_t) (
+                    reverse(IORD(darkroom_ootx_base, (uint32_t) (18 * lighthouse + 0))) & 0xFFFF);
+            ootx.frame.ID = reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 1));
+            ootx.frame.fcal_0_phase = (uint16_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 2)) &
+                                                  0xFFFF);
+            ootx.frame.fcal_1_phase = (uint16_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 3)) &
+                                                  0xFFFF);
+            ootx.frame.fcal_0_tilt = (uint16_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 4)) &
+                                                 0xFFFF);
+            ootx.frame.fcal_1_tilt = (uint16_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 5)) &
+                                                 0xFFFF);
+            ootx.frame.unlock_count = (uint8_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 6)) &
+                                                 0xFF);
+            ootx.frame.hw_version = (uint8_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 7)) &
+                                               0xFF);
+            ootx.frame.fcal_0_curve = (uint16_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 8)) &
+                                                  0xFFFF);
+            ootx.frame.fcal_1_curve = (uint16_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 9)) &
+                                                  0xFFFF);
+            uint32_t acc = reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 10));
+            ootx.frame.accel_dir_x = ((int8_t) (acc >> 0 & 0xFF));
+            ootx.frame.accel_dir_y = ((int8_t) (acc >> 8 & 0xFF));
+            ootx.frame.accel_dir_z = ((int8_t) (acc >> 16 & 0xFF));
+            ootx.frame.fcal_0_gibphase = (uint16_t) (
+                    reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 11)) & 0xFFFF);
+            ootx.frame.fcal_1_gibphase = (uint16_t) (
+                    reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 12)) & 0xFFFF);
+            ootx.frame.fcal_0_gibmag = (uint16_t) (
+                    reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 13)) & 0xFFFF);
+            ootx.frame.fcal_1_gibmag = (uint16_t) (
+                    reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 14)) & 0xFFFF);
+            ootx.frame.mode = (uint8_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 15)) & 0xFF);
+            ootx.frame.faults = (uint8_t) (reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 16)) & 0xFF);
+
+            uint32_t crc32checksum = reverse(IORD(darkroom_ootx_base, 18 * lighthouse + 17));
+
+            CRC32 crc;
+            for (int i = 0; i < 33; i++) {
+                crc.update(ootx.data[i]);
+            }
+
+            uint32_t crc32checksumCalculated = crc.finalize();
+            if (crc.finalize() == crc32checksum) { // kinda paranoid right...?!
+                roboy_middleware_msgs::DarkRoomOOTX msg;
+                msg.lighthouse = lighthouse;
+                msg.fw_version = ootx.frame.fw_version;
+                msg.id = ootx.frame.ID;
+                half fcal_0_phase, fcal_1_phase, fcal_0_tilt, fcal_1_tilt;
+                fcal_0_phase.data_ = ootx.frame.fcal_0_phase;
+                fcal_1_phase.data_ = ootx.frame.fcal_1_phase;
+                fcal_0_tilt.data_ = ootx.frame.fcal_0_tilt;
+                fcal_1_tilt.data_ = ootx.frame.fcal_1_tilt;
+                msg.fcal_0_phase = fcal_0_phase;
+                msg.fcal_1_phase = fcal_1_phase;
+                msg.fcal_0_tilt = fcal_0_tilt;
+                msg.fcal_1_tilt = fcal_1_tilt;
+                msg.unlock_count = fcal_1_tilt;
+                msg.hw_version = ootx.frame.hw_version;
+                half fcal_0_curve, fcal_1_curve;
+                fcal_0_curve.data_ = ootx.frame.fcal_0_curve;
+                fcal_1_curve.data_ = ootx.frame.fcal_1_curve;
+                msg.fcal_0_curve = fcal_0_curve;
+                msg.fcal_1_curve = fcal_1_curve;
+                // max/min value of acceleration is 127/-127, so we divide to get an orientation vector
+                msg.accel_dir_x = ootx.frame.accel_dir_x / 127.0f;
+                msg.accel_dir_y = ootx.frame.accel_dir_y / 127.0f;
+                msg.accel_dir_z = ootx.frame.accel_dir_z / 127.0f;
+                half fcal_0_gibphase, fcal_1_gibphase, fcal_0_gibmag, fcal_1_gibmag;
+                fcal_0_gibphase.data_ = ootx.frame.fcal_0_gibphase;
+                fcal_1_gibphase.data_ = ootx.frame.fcal_1_gibphase;
+                fcal_0_gibmag.data_ = ootx.frame.fcal_0_gibmag;
+                fcal_1_gibmag.data_ = ootx.frame.fcal_1_gibmag;
+                msg.fcal_0_gibphase = fcal_0_gibphase;
+                msg.fcal_1_gibphase = fcal_1_gibphase;
+                msg.fcal_0_gibmag = fcal_0_gibmag;
+                msg.fcal_1_gibmag = fcal_1_gibmag;
+                msg.mode = ootx.frame.mode;
+                msg.faults = ootx.frame.faults;
+                msg.crc32 = crc32checksum;
+
+                darkroom_ootx_pub.publish(msg);
+            } else {
+                successfully_decoded_ootx = false;
+
+                stringstream str;
+                str << "received ootx frame" << " with crc " << crc32checksum
+                    << " which does not match the calculated: " << crc32checksumCalculated << endl;
+                str << "fw_version:          " << ootx.frame.fw_version << endl;
+                str << "ID:                  " << ootx.frame.ID << endl;
+                str << "fcal_0_phase:        " << ootx.frame.fcal_0_phase << endl;
+                str << "fcal_1_phase:        " << ootx.frame.fcal_1_phase << endl;
+                str << "fcal_0_tilt:         " << ootx.frame.fcal_0_tilt << endl;
+                str << "fcal_1_tilt:         " << ootx.frame.fcal_1_tilt << endl;
+                str << "unlock_count:        " << (uint) ootx.frame.unlock_count << endl;
+                str << "hw_version:          " << (uint) ootx.frame.hw_version << endl;
+                str << "fcal_0_curve:        " << ootx.frame.fcal_0_curve << endl;
+                str << "fcal_1_curve:        " << ootx.frame.fcal_1_curve << endl;
+                str << "accel_dir_x:         " << (int) ootx.frame.accel_dir_x << endl;
+                str << "accel_dir_y:         " << (int) ootx.frame.accel_dir_y << endl;
+                str << "accel_dir_z:         " << (int) ootx.frame.accel_dir_z << endl;
+                str << "fcal_0_gibphase:     " << ootx.frame.fcal_0_gibphase << endl;
+                str << "fcal_1_gibphase:     " << ootx.frame.fcal_1_gibphase << endl;
+                str << "fcal_0_gibmag:       " << ootx.frame.fcal_0_gibmag << endl;
+                str << "fcal_1_gibmag:       " << ootx.frame.fcal_1_gibmag << endl;
+                str << "mode:                " << (uint) ootx.frame.mode << endl;
+                str << "faults:              " << (uint) ootx.frame.faults << endl;
+                ROS_DEBUG_STREAM(str.str());
+            }
+        }
+        if (!successfully_decoded_ootx) {
+            // no valid ootx frame decoded, lets switch to another sensor channel
+            ootx_sensor_channel++;
+            if (ootx_sensor_channel > NUM_SENSORS)
+                ootx_sensor_channel = 0;
+                IOWR(darkroom_ootx_base, 0, ootx_sensor_channel);
+        }
+
+        roboy_middleware_msgs::DarkRoomStatus status_msg;
+        status_msg.object_id = ethaddr;
+        uint active_sensors = 0;
+        for (uint8_t i = 0; i < NUM_SENSORS; i++) {
+            int32_t val = IORD(darkroom_base, (1 << 8 | i));
+            status_msg.sensor_state.push_back(val & 0xF);
+            if (status_msg.sensor_state.back() == 1)
+                active_sensors++;
+        }
+        ROS_INFO("lighthouse sensors active %d/%d", active_sensors, NUM_SENSORS);
+        darkroom_status_pub.publish(status_msg);
+
+        rate.sleep();
+    }
+}
+
 int32_t *h2p_lw_led_addr;
 int32_t *h2p_lw_adc_addr;
 int32_t *h2p_lw_switches_addr;
 int32_t *h2p_lw_msj_platform;
 vector<int32_t*> h2p_lw_i2c;
+int32_t *h2p_lw_darkroom;
+int32_t *h2p_lw_darkroom_ootx;
 
 void SigintHandler(int sig)
 {
@@ -252,8 +430,18 @@ int main(int argc, char *argv[]) {
 #else
     h2p_lw_msj_platform = nullptr;
 #endif
+#ifdef DARKROOM_0_BASE
+    h2p_lw_darkroom = (int32_t*)(virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + DARKROOM_0_BASE ) & ( unsigned long)( HW_REGS_MASK )) );
+#else
+    h2p_lw_darkroom = nullptr;
+#endif
+#ifdef DARKROOMOOTXDECODER_0_BASE
+    h2p_lw_darkroom_ootx = (int32_t*)(virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + DARKROOMOOTXDECODER_0_BASE ) & ( unsigned long)( HW_REGS_MASK )) );
+#else
+    h2p_lw_darkroom_ootx = nullptr;
+#endif
 
-    MSJPlatform msjPlatform(h2p_lw_msj_platform, h2p_lw_switches_addr, h2p_lw_i2c);
+    MSJPlatform msjPlatform(h2p_lw_msj_platform, h2p_lw_switches_addr, h2p_lw_i2c, h2p_lw_darkroom, h2p_lw_darkroom_ootx);
 
 
     uint8_t mask = 0x1;
