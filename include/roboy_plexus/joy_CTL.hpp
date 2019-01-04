319bc38b5aab1bbf76e3f4f4b338a0d0f4314c24
sausy_ <michael.sausmikat@gmx.at>
2019-01-04 18:11:04 +0100

    solved compile error

--- include/roboy_plexus/joy_CTL.hpp
+++ include/roboy_plexus/joy_CTL.hpp
@@ -15,10 +15,6 @@ class Joy_CTL
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
     ros::NodeHandle nh_;
-
-    int linear_, angular_;
-    double l_scale_, a_scale_;
-    //ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
 
     int32_t *throttl_GPIO;
--- src/joy_CTL.cpp
+++ src/joy_CTL.cpp
@@ -1,34 +1,16 @@
-#include <roboyPlexus/joy_CTL.hpp>
+#include <roboy_plexus/joy_CTL.hpp>
 
 Joy_CTL::Joy_CTL(int32_t *throttl_GPIO):
-  linear_(1),
-  angular_(2),
   throttl_GPIO(throttl_GPIO)
 {
 
    ROS_INFO("Debug: %d", (IORD(throttl_GPIO, 0) & 0x1));
 
-   nh_.param("axis_linear", linear_, linear_);
-   nh_.param("axis_angular", angular_, angular_);
-   nh_.param("scale_angular", a_scale_, a_scale_);
-   nh_.param("scale_linear", l_scale_, l_scale_);
-
-
-   //vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
-
-
    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Joy_CTL::joyCallback, this);
 
- }
-
-void Joy_CTL::joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
-  //geometry_msgs::Twist twist;
-  twist.angular.z = a_scale_*joy->axes[angular_];
-  twist.linear.x = l_scale_*joy->axes[linear_];
-  //vel_pub_.publish(twist);
-  ROS_INFO("show it to me baby: %d", twist.linear.x);
-  ROS_INFO("show it to me baby: %d", twist.angular.z);
 }
 
-
-//Joy_CTL teleop_turtle;
+void Joy_CTL::joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
+  //ROS_INFO("show it to me baby: %d", joy.);
+  ROS_INFO("TODO:");
+}
